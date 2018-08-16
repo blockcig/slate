8276171fca85115ade00b892b82e91a5228a1c00
Gediminas <slatedeveloper@gmail.com>
2018-08-16 12:10:38 +1000

    GUI: Remove SubstituteFonts
    Reference:
    
    https://github.com/bitcoin/bitcoin/commit/68c272527f8eb9cc8e7699bbb8956512e5b2e42d

--- src/qt/guiutil.h
+++ src/qt/guiutil.h
@@ -122,9 +122,6 @@ void openMNConfigfile();
 // Browse backup folder
 void showBackups();
 
-// Replace invalid default fonts with known good ones
-void SubstituteFonts(const QString& language);
-
 /** Qt event filter that intercepts ToolTipChange events, and replaces the tooltip with a rich text
       representation if needed. This assures that Qt can word-wrap long tooltip messages.
       Tooltips longer than the provided size threshold (in characters) are wrapped.
